f01bee45c2cfb3298fd8d509c5200d674bd08bfb
Natalie Finnegan <nfinneg2@gmail.com>
2021-03-30 21:51:43 -0600

    prmptd two questions and gnrte readme from hrdcode

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFileSync(path.join(__dirname, fileName), `${data}`, (err) => {
+    err ? console.log(err) : console.log("successfully created Readme!");
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    console.log(data);
+    const fileName = "ReadMeTEST.md";
+    writeToFile(fileName, JSON.stringify(data));
+  });
+}
 
 // Function call to initialize app
 init();
