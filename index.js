d4be37bbf59fb74f9ed5b310dc5bb06b61ba37b1
Natalie Finnegan <nfinneg2@gmail.com>
2021-03-31 15:36:22 -0600

    added rest of Qs and got data to generate in file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -15,6 +16,54 @@ const questions = [
     name: "email",
     message: "What is your email address?",
   },
+  {
+    type: "input",
+    name: "projName",
+    message: "What is your projects name?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project",
+  },
+  {
+    type: "input",
+    name: "challenges",
+    message: "What challenges did you encounter?",
+  },
+  {
+    type: "input",
+    name: "futureDev",
+    message: "Do you have directions for future development?",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPLv3", "ISC", "None"],
+  },
+  {
+    type: "input",
+    name: "installDep",
+    message: "What command should be run to install dependencies?",
+    default: "npm i",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "What command should be run to run tests?",
+    default: "npm test",
+  },
+  {
+    type: "input",
+    name: "repoKnow",
+    message: "What does the user need to know about using this repo?",
+  },
+  {
+    type: "input",
+    name: "contribute",
+    message: "What does the user need to know about contributing to this repo?",
+  },
 ];
 
 // TODO: Create a function to write README file
@@ -26,12 +75,21 @@ function writeToFile(fileName, content) {
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt(questions).then((data) => {
-    console.log(data);
-    const content = JSON.stringify(data);
-    const fileName = "ReadMeTEST.md";
-    writeToFile(fileName, content);
-  });
+  inquirer
+    .prompt(questions)
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .then(function (data) {
+      const content = JSON.stringify(data);
+      const fileName = "/ReadMeTEST.md";
+      writeToFile(fileName, content);
+    })
+    .catch(
+      (err) =>
+        err ? console.log(err) : console.log("successfully created Readme!") //why doesn't this console log
+    );
 }
 
 // Function call to initialize app
